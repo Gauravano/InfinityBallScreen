78c9f87554970d55a77c72b49b447e7c8dabe502
gauravano <sachdeva.gaurav1997@gmail.com>
2017-07-05 02:18:36 +0530

    sound and gif added

--- infi.js
+++ infi.js
@@ -8,14 +8,14 @@ function initial(){
 
 	var maxHeight = document.body.clientHeight;
 	var maxWidth = document.body.clientWidth;
-	ball.left = Math.round(Math.random()*maxWidth-30);
-    ball.top = Math.round(Math.random()*maxHeight-30);
+	ball.left = Math.round(Math.random()*maxWidth-100);
+    ball.top = Math.round(Math.random()*maxHeight-100);
     console.log(ball.top)
 	
-		ball.collision = 0;
-   		ball.mass = 1;
-      	ball.xspeed = Math.round(Math.random()*8-4);
-   		ball.yspeed = Math.round(Math.random()*8-4);
+	ball.collision = 0;
+   	ball.mass = 1;
+   	ball.xspeed = Math.round(Math.random()*8-4);
+   	ball.yspeed = Math.round(Math.random()*8-4);
 					
 }
 
@@ -26,21 +26,29 @@ function moveBall()
 			{
 				ball.left = 0;
 				ball.xspeed *= -1;
+				var audio = new Audio('39459__THE_bizniss__laser.wav');
+				audio.play();
 			}		// BOUNDRIES Left
 			
-			if (ball.left > window.innerWidth-30) {
-				ball.left = window.innerWidth-30;
+			if (ball.left > window.innerWidth-100) {
+				ball.left = window.innerWidth-100;
 				ball.xspeed *= -1;
+				var audio = new Audio('39459__THE_bizniss__laser.wav');
+				audio.play();
 			}	// BOUNDRIES Right
 			
 			if (ball.top<0) {
 				ball.top = 0;
 				ball.yspeed *= -1;
+				var audio = new Audio('39459__THE_bizniss__laser.wav');
+				audio.play();
 			}		// BOUNDRIES Top
 			
-			if (ball.top>window.innerHeight-30) {
-				ball.top = innerHeight-30;
+			if (ball.top>window.innerHeight-100) {
+				ball.top = innerHeight-100;
 				ball.yspeed *= -1;
+				var audio = new Audio('39459__THE_bizniss__laser.wav');
+				audio.play();
 			}	// BOUNDRIES Bottom
 			
 			//GET THE NEW POSITION
@@ -48,8 +56,8 @@ function moveBall()
 			ball.top += ball.yspeed;
 			
 			//APPLY THE NEW POSITION
-			//ballsArray[i].style.WebkitTransform = "translate("+ballsArray[i].left+"px,"+ballsArray[i].top+"px)";//2D Transform
-			ball.style.WebkitTransform = "translate3D("+ball.left+"px,"+ball.top+"px,0px)";//3D Transform fo better Performance?? -> "testing"            ballsArray[i].style.MozTransform = "translate3D("+ballsArray[i].left+"px,"+ballsArray[i].top+"px,0px)";
+		    //2D Transform
+			ball.style.WebkitTransform = "translate3D("+ball.left+"px,"+ball.top+"px,0px)";//3D Transform fo better Performance
 	},5);
-   		//ball.style.WebkitTransform = "translate("+ball.dx+"px,"+ball.dy+"px)";
+   		
 }
