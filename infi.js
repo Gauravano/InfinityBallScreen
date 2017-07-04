782e2a83ba1da145f159423d7ba64e8c296af4d8
gauravano <sachdeva.gaurav1997@gmail.com>
2017-07-04 21:02:28 +0530

    simply implemented , styles left

--- infi.js
+++ infi.js
@@ -1,9 +1,15 @@
 var ball = document.getElementById('infiBall');
+initial();
+
+
+moveBall();
 
 function initial(){
 
-	ball.left = Math.round(Math.random()*window.innerWidth-30);
-    ball.top = Math.random() * (i*minDist+50 - i*minDist) + i*minDist;
+	var maxHeight = document.body.clientHeight;
+	var maxWidth = document.body.clientWidth;
+	ball.left = Math.round(Math.random()*maxWidth-30);
+    ball.top = Math.round(Math.random()*maxHeight-30);
     console.log(ball.top)
 	
 		ball.collision = 0;
@@ -11,4 +17,39 @@ function initial(){
       	ball.xspeed = Math.round(Math.random()*8-4);
    		ball.yspeed = Math.round(Math.random()*8-4);
 					
+}
+
+function moveBall()
+{		
+	setInterval(function(){
+			if (ball.left<0) 
+			{
+				ball.left = 0;
+				ball.xspeed *= -1;
+			}		// BOUNDRIES Left
+			
+			if (ball.left > window.innerWidth-30) {
+				ball.left = window.innerWidth-30;
+				ball.xspeed *= -1;
+			}	// BOUNDRIES Right
+			
+			if (ball.top<0) {
+				ball.top = 0;
+				ball.yspeed *= -1;
+			}		// BOUNDRIES Top
+			
+			if (ball.top>window.innerHeight-30) {
+				ball.top = innerHeight-30;
+				ball.yspeed *= -1;
+			}	// BOUNDRIES Bottom
+			
+			//GET THE NEW POSITION
+			ball.left += ball.xspeed;
+			ball.top += ball.yspeed;
+			
+			//APPLY THE NEW POSITION
+			//ballsArray[i].style.WebkitTransform = "translate("+ballsArray[i].left+"px,"+ballsArray[i].top+"px)";//2D Transform
+			ball.style.WebkitTransform = "translate3D("+ball.left+"px,"+ball.top+"px,0px)";//3D Transform fo better Performance?? -> "testing"            ballsArray[i].style.MozTransform = "translate3D("+ballsArray[i].left+"px,"+ballsArray[i].top+"px,0px)";
+	},5);
+   		//ball.style.WebkitTransform = "translate("+ball.dx+"px,"+ball.dy+"px)";
 }
